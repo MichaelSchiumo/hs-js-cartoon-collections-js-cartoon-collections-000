592015878979e0badf79628a291b98d6825c859b
Michael J. Schiumo <mschiumo18@gmail.com>
2018-10-27 01:51:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+function dwarfRollCall(call) {
+var count = '';
+var i = 0
+call.forEach(function(dwarf,i) {
+count += `${i + 1}. ${dwarf}`;  
+count += " "
+});
+{ return `${count}`
+}}
+
+
+
+var calls = ["earth","wind","fire","water","heart"]
+
+function summonCaptainPlanet(calls) {
+var n = 0;
+while (n < calls.length) {
+calls[n] = `${calls[n].toUpperCase()}!`;
+n++;
+} 
+return calls
+}
+
+function longPlaneteerCalls(planeteerCalls) {
+var t = planeteerCalls.length;
+if (t < 4) {
+return false  
+}
+else { return true
+}
+}
+
 
 function findTheCheese(ingredients) {
 var cheeses = ["cheddar","gouda","camembert"];
 for(let i = 0; i < ingredients.length; i++) {
-cheeses.includes(ingredients[i])
+if (cheeses.includes(ingredients[i])) {
 return ingredients[i]
+}
 }
 return 'no cheese!'
 } 
