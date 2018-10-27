20e91942fe5fdbc5f119eaeb64f42935f3655c2a
Michael J. Schiumo <mschiumo18@gmail.com>
2018-10-27 01:45:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function dwarfRollCall(dwarves) {
-}
 
-function summonCaptainPlanet(planeteerCalls){
+function findTheCheese(ingredients) {
+var cheeses = ["cheddar","gouda","camembert"];
+for(let i = 0; i < ingredients.length; i++) {
+cheeses.includes(ingredients[i])
+return ingredients[i]
 }
+return 'no cheese!'
+} 
 
-function longPlaneteerCalls(words) {
-}
 
-function findTheCheese (foods) {
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
